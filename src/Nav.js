50f639d5b619bcdba9988314a6a3054ee68a36f9
saleborg <Stefan.aleborg@gmail.com>
2022-01-17 12:30:48 +0100

    netflix site

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="https://assets.brand.microsites.netflix.io/assets/7dc497e2-4975-11ec-a9ce-066b49664af6_cm_1440w.jpg?v=1"
+        alt="Netflix Logo"
+        className="nav__logo"
+      />
+      <img
+        src="https://images.vexels.com/media/users/3/129616/isolated/preview/fb517f8913bd99cd48ef00facb4a67c0-businessman-avatar-silhouette-by-vexels.png"
+        alt="Avatar"
+        className="nav__avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "4b2f8b7f96d68b7bac22c09b0e776e53";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
